014a48de752c7674014b8c04aff434f1f3982fcd
Haroon <walkin.logic@gmail.com>
2023-08-21 18:37:56 +0500

    #update success page design

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -25,18 +25,20 @@ const Register = () => {
   };
 
   return (
-    <div className="container container-fluid">
-      <div className="row mt-5 d-flex justify-content-center">
-        <div className="col-10 col-lg-5 ">
-           
-            <h1 className="mb-4">Success</h1>
-
+    <main className="">
+      <div className="my-10">
+        <div className="container container-fluid">
+          <div className="row mt-5 d-flex justify-content-center">
+            <div className="col-10 col-lg-5 ">
+              <h1 className="mb-4">Success</h1>
               <p className="text-gray-950 h-fit max-w-sm">
-				Thank you for your order from Khaadi. Once your package ships we will send an email with a link to track your order.
-			  </p>
+                Thank you for your order from Khaadi. Once your package ships we will send an email with a link to track your order.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
